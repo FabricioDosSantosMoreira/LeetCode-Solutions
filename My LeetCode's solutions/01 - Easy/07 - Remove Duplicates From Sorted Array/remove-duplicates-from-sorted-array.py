12dba2b4bc16e71bc41cf0b0e9919465486fac4f
Fabrício Dos Santos Moreira <dev.fabriciodossantosmoreira@gmail.com>
2024-08-25 20:32:47 -0300

    [ADD] - Remove Duplicates From Sorted Array

--- My LeetCode's solutions/01 - Easy/07 - Remove Duplicates From Sorted Array/remove-duplicates-from-sorted-array.py
+++ My LeetCode's solutions/01 - Easy/07 - Remove Duplicates From Sorted Array/remove-duplicates-from-sorted-array.py
@@ -4,12 +4,14 @@ from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        # NOTE: 
+        # NOTE: Time Complexity = O(n log n) 
+        # 'n' is the length of the list 'nums'
         nums[:] = sorted(set(nums))
         return len(nums)
 
 
-        # NOTE:
+        # NOTE: Time Complexity = O(n^2)
+        # 'n' is the length of the list 'nums'
         # i: int = 0
         # while i < len(nums) - 1:
         #     if nums[i] == nums[i + 1]:
@@ -19,6 +21,20 @@ class Solution:
         #         i += 1
 
         # return len(nums)
+
+
+        # NOTE: Time Complexity = O(n)
+        # 'n' is the length of the list 'nums'
+        # if not nums:
+        #     return 0
+
+        # slow: int = 0
+        # for fast in range(1, len(nums)):
+        #     if nums[fast] != nums[slow]:
+        #         slow += 1
+        #         nums[slow] = nums[fast]
+
+        # return slow + 1
 
 
 solution = Solution()
